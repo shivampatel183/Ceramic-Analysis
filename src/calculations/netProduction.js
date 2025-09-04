ae0d5d1e3955fe75ccff3c06368f4ca9484a752f
Shivam <shivamarvadiya@gmail.com>
2025-09-04 10:14:36 +0530

    #

--- src/calculations/netProduction.js
+++ src/calculations/netProduction.js
@@ -43,9 +43,6 @@ export async function fetchProductionBySize(filter, applyDateFilter) {
 
   const { data } = await query;
   return groupBySize(data);
-}
-
-function groupBySize(data) {
   if (!data || data.length === 0) {
     return [];
   }
