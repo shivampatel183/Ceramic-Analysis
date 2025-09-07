f01979bc02e3032d9b98c6de8d36d347642c1197
Shivam <shivamarvadiya@gmail.com>
2025-09-07 23:39:34 +0530

    #

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,9 @@
-
 export function Card({ children, className }) {
   return (
     <div className={`bg-white shadow rounded-lg p-4 ${className}`}>
       {children}
     </div>
   );
-}
-
-export function CardHeader({ children }) {
-  return <div className="mb-2 font-bold text-lg">{children}</div>;
 }
 
 export function CardContent({ children }) {
