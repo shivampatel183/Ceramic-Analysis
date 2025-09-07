53e143df2039d8cd8991b958a8bedbbef590d9c2
Shivam <shivamarvadiya@gmail.com>
2025-09-07 23:39:49 +0530

    #

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,10 @@ export function Card({ children, className }) {
   );
 }
 
+export function CardHeader({ children }) {
+  return <div className="mb-2 font-bold text-lg">{children}</div>;
+}
+
 export function CardContent({ children }) {
   return <div>{children}</div>;
 }
