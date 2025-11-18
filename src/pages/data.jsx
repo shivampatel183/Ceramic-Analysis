aa1c84940a3ac44563541c17300e89d2d52a139f
Shivam <shivamarvadiya@gmail.com>
2025-11-18 21:02:49 +0530

    #

--- src/pages/data.jsx
+++ src/pages/data.jsx
@@ -66,6 +66,9 @@ const formatDate = (value) => {
   const d = new Date(value);
 
   const userTimezoneOffset = d.getTimezoneOffset() * 60000;
+  const correctedDate = new Date(d.getTime() + userTimezoneOffset);
+  return correctedDate.toLocaleDateString("en-GB");
+};
 
 export default function DataTable({ userRole, userDepartment }) {
   const [rows, setRows] = useState([]);
